c13d9a36f064fba66d07e0d319c9d7fb67b09ac8
daulat <daulatjajra2006@gmail.com>
2024-01-04 10:30:26 +0530

    'movements'

--- script.js
+++ script.js
@@ -62,3 +62,20 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    let type = mov > 0 ? "deposit" : "withdrawal";
+    let html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type} </div>
+          <div class="movements__value">${mov}</div>
+        </div>`;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
+console.log(containerMovements.innerHTML);
